cc3faac69b9b4e3c4b4baad746e23412388fc358
David Page <34013090+dbpage03@users.noreply.github.com>
2019-04-25 11:33:47 -0500

    4/25/19 - Looking + Movement

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,6 +4,7 @@ onready var camFP = self.get_parent().get_node("FP")
 onready var camTP = self.get_parent().get_node("TP")
 const SPEED = 4
 const ROTSPEED = 4
+var play = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,28 +30,46 @@ func backward():
 	x = -cos(a) * SPEED
 	self.move_and_slide(Vector3(x,0,z))
 
+func sideways(way):
+	var a = degRad(self.rotation_degrees.y)
+	var x
+	var z
+	z = sin(a) * SPEED * way
+	x = -cos(a) * SPEED * way
+	self.move_and_slide(Vector3(x,0,z))
+	pass
+
 func mouse():
 	var x = -(get_viewport().get_mouse_position().x - 200) / 10 * ROTSPEED
 	var y = (get_viewport().get_mouse_position().y - 200) / 10 * ROTSPEED
 	camFP.rotate_y(degRad(x))
 	var t = camFP.get_transform()
-	t.basis = Basis(t.basis[0], deg2rad(-y))*t.basis
+	t.basis = Basis(t.basis[0].normalized(), deg2rad(-y))*t.basis
 	camFP.set_transform(t)
-	get_viewport().warp_mouse(Vector2(200,200))
+	if play:
+		get_viewport().warp_mouse(Vector2(200,200))
+
+func _notification(what):
+	if what == MainLoop.NOTIFICATION_WM_FOCUS_OUT:
+		play = false
+	if what == MainLoop.NOTIFICATION_WM_FOCUS_IN:
+		play = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.rotation.y = camFP.rotation.y
-	camFP.translation.x = self.translation.x
-	camFP.translation.z = self.translation.z
-	camFP.translation.y = self.translation.y
-	
-	if Input.is_action_pressed("game_left"):
-		camFP.rotate_y(degRad(ROTSPEED))
-	if Input.is_action_pressed("game_right"):
-		camFP.rotate_y(degRad(-ROTSPEED))
-	if Input.is_action_pressed("game_up"):
-		forward()
-	if Input.is_action_pressed("game_down"):
-		backward()
-	mouse()
+	if play:
+		self.rotation.y = camFP.rotation.y
+		camFP.translation.x = self.translation.x
+		camFP.translation.z = self.translation.z
+		camFP.translation.y = self.translation.y
+		
+		if Input.is_action_pressed("game_left"):
+			sideways(1)
+		if Input.is_action_pressed("game_right"):
+			sideways(-1)
+		if Input.is_action_pressed("game_up"):
+			forward()
+		if Input.is_action_pressed("game_down"):
+			backward()
+		mouse()
+		
