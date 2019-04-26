d892be0e795aba457849be735f960154aa074995
David Page <34013090+dbpage03@users.noreply.github.com>
2019-04-26 14:26:12 -0500

    4/26/19 - More New Movement

--- scripts/New_Player.gd
+++ scripts/New_Player.gd
@@ -6,7 +6,7 @@ const JUMP_SPEED = 10
 const ACCEL = 4.5
 const DEACCEL = 16
 const MAX_SLOPE_ANGLE = 40
-var velocity = Vector3()
+var vel = Vector3()
 
 var camFP
 
@@ -16,10 +16,37 @@ func _ready():
 	pass
 
 func _physics_process(delta):
+	process_input(delta)
+	process_movement(delta)
 	pass
 
 func process_input(delta):
-	pass
+	var cam_xform = camFP.get_global_transform()
+	var input_movement_vector = Vector2()
+	
+	if Input.is_action_pressed("game_up"):
+		input_movement_vector.y += 1
+	if Input.is_action_pressed("game_down"):
+		input_movement_vector.y -= 1
+	if Input.is_action_pressed("game_left"):
+		input_movement_vector.x -= 1
+	if Input.is_action_pressed("game_right"):
+		input_movement_vector.x += 1
+	
+	input_movement_vector = input_movement_vector.normalized()
+	dir += -cam_xform.basis.z.normalized() * input_movement_vector.y
+	dir += cam_xform.basis.x.normalized() * input_movement_vector.x
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("movement_jump"):
+			vel.y = JUMP_SPEED
+	
+	# Capturing/Freeing the cursor
+	if Input.is_action_just_pressed("ui_cancel"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func process_movement(delta):
 	pass
