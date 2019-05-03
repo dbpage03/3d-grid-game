0dbf6cbd1309b6a744489f25351716b8aaaa7ed4
David Page <34013090+dbpage03@users.noreply.github.com>
2019-05-02 22:33:09 -0500

    5/2/19 - Terrain Gen

--- scripts/Generator.gd
+++ scripts/Generator.gd
@@ -1,10 +1,21 @@
 extends GridMap
 
-
-
 func _ready():
+	dirtLayer(3)
 	pass
 
-func dirtLayer():
+func dirtLayer(var layers):
+	var p = self.get_parent().get_node("Player").get_global_transform().origin
+	
+	for y in range(0,layers-1):
+		for x in range(-50,50):
+			for z in range(-50,50):
+				self.set_cell_item(x,y,z,1)
+	grassLayer(layers)
 	pass
 
+func grassLayer(var layer):
+	for x in range(-50,50):
+		for z in range(-50,50):
+			self.set_cell_item(x,layer,z,2)
+	pass
--- scripts/New_Player.gd
+++ scripts/New_Player.gd
@@ -3,12 +3,17 @@ extends KinematicBody
 const GRAVITY = -32
 const MAX_SPEED = 8
 const JUMP_SPEED = 15
+const SPRINT_MULT = 1.8
+const SNEAK_MULT = 0.4
 const MAX_SLOPE_ANGLE = 40
 var vel = Vector3()
 onready var camFP = self.get_parent().get_node("FP")
 var MOUSE_SENSITIVITY = 0.2
 var dir = Vector3() 
+#warning-ignore:unused_class_variable
 var play = false
+var sprint = false
+var sneak = false
 
 func _ready():
 	pass
@@ -18,8 +23,8 @@ func _physics_process(delta):
 	process_movement(delta)
 	pass
 
+#warning-ignore:unused_argument
 func process_input(delta):
-	var cam_xform = camFP.get_global_transform()
 	var input_movement_vector = Vector2()
 	dir = Vector3()
 	
@@ -31,6 +36,15 @@ func process_input(delta):
 		input_movement_vector.x += 1
 	if Input.is_action_pressed("game_right"):
 		input_movement_vector.x -= 1
+	
+	if Input.is_action_pressed("game_sprint"):
+		sprint = true
+	else:
+		sprint = false
+	if Input.is_action_pressed("game_sneak"):
+		sneak = true
+	else:
+		sneak = false
 	
 	dir.x = input_movement_vector.x
 	dir.z = input_movement_vector.y
@@ -49,13 +63,19 @@ func process_input(delta):
 func process_movement(delta):
 	vel.y += GRAVITY * delta
 	
+	var speed_multiplier = 1
+	if sneak:
+		speed_multiplier = SNEAK_MULT
+	elif sprint:
+		speed_multiplier = SPRINT_MULT
+	
 	var a = deg2rad(self.rotation_degrees.y + 90)
-	vel.z = -sin(a) * MAX_SPEED * dir.z
-	vel.x = cos(a) * MAX_SPEED * dir.z
+	vel.z = -sin(a) * MAX_SPEED * dir.z * speed_multiplier
+	vel.x = cos(a) * MAX_SPEED * dir.z * speed_multiplier
 	
 	a = deg2rad(self.rotation_degrees.y)
-	vel.z += sin(a) * MAX_SPEED * dir.x
-	vel.x += -cos(a) * MAX_SPEED * dir.x
+	vel.z += sin(a) * MAX_SPEED * dir.x * speed_multiplier
+	vel.x += -cos(a) * MAX_SPEED * dir.x * speed_multiplier
 	
 	vel = move_and_slide(vel, Vector3(0,1,0), 0.05, 4, deg2rad(MAX_SLOPE_ANGLE))
 	camFP.translation.x = self.translation.x
